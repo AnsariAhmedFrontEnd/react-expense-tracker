2611225dc543561eca7e95310acb585460bac293
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-08-31 00:12:52 +0530

    Added Login and redirect functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Fragment } from "react";
-import Login from "./components/Login";
+import Login from "./Pages/Login";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./Pages/Welcom";
 
 const App = () => {
+  const token = localStorage.getItem('token');
+
+  const isLoggedIn = !!token
   return (
-   <Fragment>
-    <Login />
-   </Fragment>
+   <Routes>
+   {!isLoggedIn && <Route path="/" element={<Login />} />}
+   {isLoggedIn && <Route path="/welcome" element={<Welcome />}  />}
+    
+    </Routes>
     );
 }
 
