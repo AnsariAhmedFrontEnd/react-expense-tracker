bfa81cdcc03ff2691330b8bcf51b62eac3962f71
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-09-01 21:24:27 +0530

    Get User Data

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import Login from "./Pages/Login";
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./Pages/Welcom";
 import Profile from "./Pages/Profile";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
+import Layout from "./components/Layout";
 
 const App = () => {
-  const token = localStorage.getItem('token');
+  const authCtx = useContext(AuthContext);
 
-  const isLoggedIn = !!token
+  const isLoggedIn = authCtx.isLoggedIn;
+  
   return (
-   <Routes>
-   {!isLoggedIn && <Route path="/" element={<Login />} />}
-   {isLoggedIn && <Route path="/welcome" element={<Welcome />}  />}
-   {isLoggedIn && <Route path="/profile" element={<Profile />}  />}
-    
+    <Layout>
+    <Routes>
+      {!isLoggedIn && <Route path="/" element={<Login />} />}
+      {isLoggedIn && <Route path="/welcome" element={<Welcome />} />}
+      {isLoggedIn && <Route path="/profile" element={<Profile />} />}
     </Routes>
-    );
-}
+    </Layout>
+  );
+};
 
 export default App;
